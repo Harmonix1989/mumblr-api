e9d02f058d39fcd6edc502bc177e6f9eb3a5c8f7
Harmonix1989 <mayes.darren@gmail.com>
2025-06-20 17:03:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
 import openai
 import os
 
 app = Flask(__name__)
+CORS(app)  # This enables CORS for all routes
 
 SYSTEM_PROMPT = """
 Mumblr expects input fields such as:
